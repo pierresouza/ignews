91b94442c07daab2e8e8439e0c67661f66c261ce
Pierre Souza <pas@lionx.com.br>
2022-10-03 16:41:44 -0300

    chore: finish project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+// Client-side
+// Server-side
+// Static Site Generation
+
+// Post do blog
+
+//Conteudo(SSG)
+// Comentários (Client-side)
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,13 +35,36 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
-
-        <img src="/images/avatar.svg" alt="girl coding" />
+        <Image
+          width={336}
+          height={521}
+          src="/images/avatar.svg"
+          alt="girl coding"
+        />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LotIiBiOQQoaivHHgmSkiem');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  };
+};
